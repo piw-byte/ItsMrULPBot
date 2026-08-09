7dcf358e267a7027f83da43b502daebf4e541ca8
piw-byte <petermosquito13@gmail.com>
2026-08-09 09:39:23 +0800

    config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 #Copyright @ISmartCoder
 #Updates Channel @abirxdhackz
 API_ID = 37870587
-API_HASH = '6ed6b3032de36d16ec21e5c7030fdd53''
+API_HASH = '6ed6b3032de36d16ec21e5c7030fdd53'
 BOT_TOKEN = '8877509658:AAGksdG2oI_ewoPTWjz7ojt_BE86litpWzo'
 UPDATE_CHANNEL_URL = 'https://t.me/mwpan'
 COMMAND_PREFIXES = ['/', '!', '.', ',', '$', '#']
